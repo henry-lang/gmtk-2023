e9180d60611acc972ea177c7c6a2e5e4a2c9979a
henry-lang <henry.langmack@gmail.com>
2023-07-08 14:35:47 -0400

    jump pad and bouncing off of walls works

--- Assets/Components/PlayerMovement.cs
+++ Assets/Components/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using UnityEngine;
 
@@ -6,27 +5,25 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public float movementSpeed = 100f;
     private Rigidbody2D _rb;
-    private BoxCollider2D _collider;
+    private Vector2 _dir = Vector2.right;
         
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
     {
-        _rb.velocity = new Vector2(movementSpeed * Time.deltaTime, _rb.velocity.y);
+        _rb.velocity = movementSpeed * Time.deltaTime * _dir + new Vector2(0, _rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var normal = collision.GetContact(0).normal;
+        Debug.Log(normal);
         if (new[]{Vector2.left, Vector2.right}.Contains(normal) && collision.gameObject.CompareTag("Ground"))
         {
-            var velocity = _rb.velocity;
-            velocity = new Vector2(-velocity.x, velocity.y);
-            _rb.velocity = velocity;
+            _dir *= -1;
         }
     }
 }
